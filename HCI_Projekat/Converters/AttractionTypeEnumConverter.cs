56d3bdec56452e23584c713043376dc6d5fe7be4
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-06-12 12:28:20 +0200

    copy-paste i za atrakcije tabela

---
+++ HCI_Projekat/Converters/AttractionTypeEnumConverter.cs
@@ -0,0 +1,57 @@
+﻿using HCI_Projekat.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HCI_Projekat.Converters
+{
+    internal class AttractionTypeEnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is AttractionType accomodationType)
+            {
+                switch (accomodationType)
+                {
+                    case AttractionType.Cultural:
+                        return "Kulturna";
+                    case AttractionType.Nature:
+                        return "Prirodna";
+                    case AttractionType.Entertainment:
+                        return "Zabavna";
+                    case AttractionType.Religious:
+                        return "Verska";
+                }
+            }
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                if ((string)value == "Kulturna")
+                {
+                    return AttractionType.Cultural;
+                }
+                else if ((string)value == "Prirodna")
+                {
+                    return AttractionType.Nature;
+                }
+                else if ((string)value == "Zabavna")
+                {
+                    return AttractionType.Entertainment;
+                }
+                else if ((string)value == "Verska")
+                {
+                    return AttractionType.Religious;
+                }
+            }
+            return null;
+        }
+    }
+}
--- HCI_Projekat/Pages/Tabele/AttractionsTable.xaml.cs
+++ HCI_Projekat/Pages/Tabele/AttractionsTable.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using HCI_Projekat.Commands;
+using HCI_Projekat.Forms;
+using HCI_Projekat.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +24,132 @@ namespace HCI_Projekat.Pages.Tabele
     /// <summary>
     /// Interaction logic for AttractionsTable.xaml
     /// </summary>
-    public partial class AttractionsTable : Page
+    public partial class AttractionsTable : Page, INotifyPropertyChanged
     {
+        private ObservableCollection<Attraction> _selectedItems;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Attraction> SelectedItems
+        {
+            get { return _selectedItems; }
+            set
+            {
+                _selectedItems = value;
+                OnPropertyChanged(nameof(SelectedItems));
+            }
+        }
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ICommand DeleteSelectedItemsCommand { get; }
+        public ICommand EditSelectedItemsCommand { get; }
+        public List<Attraction> Attractions { get; set; }
         public AttractionsTable()
         {
             InitializeComponent();
+            DataContext = this;
+            Repository.AppContext dbContext = new Repository.AppContext();
+            Attractions = dbContext.Attractions!.ToList();
+            DeleteSelectedItemsCommand = new RelayCommand<IEnumerable<Attraction>>(DeleteSelectedItems, CanProcessSelectedItems);
+            EditSelectedItemsCommand = new RelayCommand<IEnumerable<Attraction>>(EditSelectedItems, CanProcessSelectedItems);
+        }
+
+        private void DataGridAtrakcije_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedItems = new ObservableCollection<Attraction>(DataGridAtrakcije.SelectedItems.Cast<Attraction>());
+        }
+        private bool CanProcessSelectedItems(IEnumerable<Attraction> selectedItems)
+        {
+            return selectedItems != null && selectedItems.Any();
+        }
+        private void DeleteSelectedItems(IEnumerable<Attraction> selectedItems)
+        {
+            var msgBox = new MessageBoxCustom("Da li sigurno zelite da obrisete to?", MessageType.Confirmation, MessageButtons.YesNo);
+            msgBox.ShowDialog();
+            if ((bool)msgBox.DialogResult!)
+            {
+                using (var context = new Repository.AppContext())
+                {
+                    DbSet<Attraction> itemSet = context.Attractions!;
+
+                    foreach (Attraction selectedItem in selectedItems)
+                    {
+                        var trackedItem = itemSet.Find(selectedItem.Id);
+                        if (trackedItem != null)
+                        {
+                            itemSet.Remove(trackedItem);
+                        }
+                    }
+
+                    context.SaveChanges();
+
+                    var updatedItems = itemSet.ToList();
+
+                    // Update the UI with the updated data
+                    DataGridAtrakcije.ItemsSource = updatedItems;
+                }
+            }
+        }
+
+        private void EditSelectedItems(IEnumerable<Attraction> selectedItems)
+        {
+            foreach (Attraction selectedItem in selectedItems)
+            {
+                //TODO: Staviti odgovarajucu formu!!!
+                //AccomodationForm accomodationForm = new AccomodationForm();
+                //accomodationForm.DataContext = selectedItem;
+                //accomodationForm.Show();
+            }
+
+            using (var context = new Repository.AppContext())
+            {
+                DataGridAtrakcije.ItemsSource = context.Attractions?.ToList();
+            }
+        }
+
+        private void DataGridAtrakcije_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                e.Handled = true; // Suppress the default delete behavior
+            }
+        }
+        private void AddNewItem()
+        {
+            //TODO: Staviti odgovarajucu formu!!!
+            //AccomodationForm accomodationForm = new AccomodationForm();
+            //accomodationForm.ItemAdded += AccomodationForm_ItemAdded;
+            //accomodationForm.Show();
+        }
+        private void AccomodationForm_ItemAdded(object sender, EventArgs e)
+        {
+            using (var context = new Repository.AppContext())
+            {
+                DataGridAtrakcije.ItemsSource = context.Attractions?.ToList();
+            }
+        }
+
+        private void Page_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.E)
+            {
+                EditSelectedItemsCommand.Execute(SelectedItems);
+            }
+            else if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.N)
+            {
+                AddNewItem();
+            }
+            else if (e.Key == Key.Delete)
+            {
+                DeleteSelectedItemsCommand.Execute(SelectedItems);
+            }
+        }
+
+        private void NovoButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewItem();
         }
     }
 }
